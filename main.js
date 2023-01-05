72130e376e499240f41a5dcd22f1835dab29c3e8
John Yang <john.yang8097@gmail.com>
2023-01-04 19:05:17 -0500

    commit tree 97d886747ce03ba0f1f541dd86bde76f9a6608f6

--- main.js
+++ main.js
@@ -4,8 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.getElementById('modal');
+modal.className = 'hidden';
+const modalMsg = document.getElementById('modal-message')
 
+const hearts = document.querySelectorAll(".like-glyph"); //NodeList
 
+for (let heart of hearts) {
+  heart.addEventListener("click", toggleHeart);
+}
+
+function toggleHeart(e) {
+  // stimulate making a server req - random fails to simulate faulty network conditions
+  console.log(`${e.target.innerHTML} clicked`);
+  let heart = e.target
+  mimicServerCall()
+    .then(function(serverMessage) {
+      alert(serverMessage);
+      if (heart.innerText == EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.classList.add('.activated-heart');
+      } else {
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove('.activated-heart');
+      }
+
+    })
+    .catch((error) => {
+      // when sth is wrong in a fetch, the next catch() is called so that error work can be performed
+      // so JS won't fail silently
+      // Display the server error message in the modal
+      // alert(error);
+      modal.classList.remove('hidden')
+      modalMsg.innerHTML += error
+      setTimeout(()=> modal.className = 'hidden', 5000);
+    })
+};
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
